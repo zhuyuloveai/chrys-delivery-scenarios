dd4fe99d728350d34ca08cfa84854736a7397551
zhuyu <zhuyu4010qq@gmail.com>
2026-07-23 17:07:16 +0800

    Revert "scenario s05: add loot rarity source"
    This reverts commit c1e6a23abb4f1efea2808bc8301a39a6455cf000.

--- src/loot-rarity.js
+++
@@ -1,6 +0,0 @@
-export function lootRarity(dropRate) {
-  if (dropRate <= 0.01) return "mythic";
-  if (dropRate <= 0.05) return "epic";
-  if (dropRate <= 0.2) return "rare";
-  return "common";
-}
